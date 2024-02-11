6279bf4c87abd2399193fb73e58e0852126e930c
Katerina Mangileva <ms.katiysha@mail.ru>
2024-02-11 19:27:03 +0500

    Конфигурация для сервера и зависимости

--- conftest.py
+++ conftest.py
@@ -1,10 +1,14 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver import ChromeOptions
 
 
 @pytest.fixture
 def driver():
-    driver = webdriver.Chrome()
-    driver.maximize_window()
+    options = ChromeOptions()
+    options.add_argument("--headless")
+    driver = webdriver.Chrome(options=options)
+    driver.set_window_size(1920, 1080)
+    # driver.maximize_window()
     yield driver
     driver.quit()
