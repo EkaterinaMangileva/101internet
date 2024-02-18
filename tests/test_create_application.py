c6d85b64f1574234596a95ac3bf30be4da65af51
Katerina Mangileva <ms.katiysha@mail.ru>
2024-02-18 18:15:45 +0500

    добавила аллюр

--- tests/test_create_application.py
+++ tests/test_create_application.py
@@ -1,12 +1,19 @@
 from pages.internet_providers_page import AddressPlaceholder, ApplicationForInternetConnection
+import telebot
+
+bot = telebot.TeleBot("6528485256:AAFr27qVMMB-A_Ig_YeX3DSK-kvWBr-TR3U")
+chat_id = 456437503
 
 
 class TestOneHundredInternet:
 
     def test_application(self, driver):
-        internet_page = AddressPlaceholder(driver, "https://piter-online.net/")
-        internet_page.open()
-        internet_page.fill_in_the_address()
-        internet_providers = ApplicationForInternetConnection(driver,
-                                                              "https://piter-online.net/leningradskaya-oblast/rates")
-        internet_providers.create_application()
+        try:
+            internet_page = AddressPlaceholder(driver, "https://piter-online.net/")
+            internet_page.open()
+            internet_page.fill_in_the_address()
+            internet_providers = ApplicationForInternetConnection(driver,
+                                                                  "https://piter-online.net/leningradskaya-oblast/rates")
+            internet_providers.create_application()
+        finally:
+            bot.send_message(chat_id, "Заявки отправлены, отчет смотри здесь")
